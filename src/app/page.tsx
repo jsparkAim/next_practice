df9c0b21bbf66dae4da2bccbabd6701245e3b333
jspark <jspark@aimmed.com>
2024-04-23 10:17:06 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+import React from 'react';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import LoginButton from './ui/login/LoginButton';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items2 = new Array(3).fill(null).map((_, index) => ({
+  key: String(index + 1),
+  label: `nav ${index + 1}`,
+}));
+
+const items: MenuProps['items'] = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  AppstoreOutlined,
+  TeamOutlined,
+  ShopOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(icon),
+  label: `nav ${index + 1}`,
+}));
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <>
+    <Header
+      style={{
+        position: 'fixed',
+        top: 0,
+        zIndex: 1,
+        width: '91%',
+        display: 'flex',
+        alignItems: 'center',
+          }}
+        >
+      <LoginButton/>
+    </Header>
+    <Layout hasSider>
+      <Sider
+        style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0, top: 70, bottom: 0 }}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+      </Sider>
+      <Layout style={{ marginLeft: 200 }}>
+      
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          <div
+            style={{
+              padding: 24,
+              textAlign: 'center',
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <p>long content</p>
+            {
+              // indicates very long content
+              Array.from({ length: 100 }, (_, index) => (
+                <React.Fragment key={index}>
+                  {index % 20 === 0 && index ? 'more' : '...'}
+                  <br />
+                </React.Fragment>
+              ))
+            }
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+    </>
+  );
+};
+
+export default App;
